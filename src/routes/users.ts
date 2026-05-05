39f07c01583d2de6321269628cf37dbbfba42398
pedrodfato <pedro.casstrof@gmail.com>
2026-05-05 00:43:52 -0300

    New route and Authentication JWT

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import type { FastifyInstance } from 'fastify';
 import bcrypt from 'bcrypt';
+import { eq } from 'drizzle-orm';
 import { db } from '../db.js'; 
 import { users } from '../schema.js';
 
@@ -16,10 +17,34 @@ export async function userRoutes(app: FastifyInstance) {
                 passwordHash: hashedPassword,
             });
 
-            return reply.status(201).send({ message: 'Usuário recrutado com sucesso!' });
+            return reply.status(201).send({ message: 'Usuário criado com sucesso!' });
         } catch (error) {
-            console.error('Erro ao recrutar usuário:', error);
-            return reply.status(500).send({ message: 'Erro ao recrutar usuário' });
+            console.error('Erro ao criar usuário:', error);
+            return reply.status(500).send({ message: 'Erro ao criar usuário' });
         }
+    });
+
+    app.post('/login', async (request, reply) => {
+        const {email, password} = request.body as any;
+
+        const result = await db.select().from(users).where(eq(users.email, email));
+        const user = result[0];
+
+        if (!user) {
+            return reply.status(401).send({ message: 'Credenciais inválidas' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+        if (!isPasswordValid) {
+            return reply.status(401).send({ message: 'Credenciais inválidas' });
+        }
+
+        const token = app.jwt.sign({sub: user.id}, {expiresIn: '7d'});
+
+        return reply.send({
+            message: 'Login bem-sucedido',
+            token
+        });
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,61 +1,26 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import fastifyJwt from '@fastify/jwt';
 import { DatabasePostgres } from './database-postgres.js';
 import { userRoutes } from './routes/users.js';
+import { medicationsRoutes } from './routes/medications.js';
 
 
 const app = Fastify({logger: true})
 
-const database = new DatabasePostgres();
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET || 'chave_super_secreta_medtracker_2026'
+});
+
+
 
 app.register(cors, {
     origin: true,
 });
 
-interface RemedioQuery {
-    search?: string;
-}
 
-app.get<{ Querystring: RemedioQuery }>('/remedios', async (request) => {
-    const search = request.query.search
-    const remedios = await database.list(search)
-
-    console.log(remedios)
-    return remedios
-});
-
-interface RemedioBody {
-    name: string;
-    dosage: string;
-    total_pills: number;
-    frequency_hours: number;
-    start_date: string;
-    end_date?: string | null;
-}
-
-app.post<{ Body: RemedioBody }>('/remedios', async (request, reply) => {
-
-    await database.create(request.body as any)
-
-    return reply.status(201).send()
-})
-
-app.put<{ Params: { id: number }, Body: RemedioBody }>('/remedios/:id', async (request, reply) => {
-    const remedioId = request.params.id
-
-    await database.update(Number(remedioId), request.body as any)
-
-    return reply.status(204).send()
-})
-
-app.delete<{ Params: { id: number } }>('/remedios/:id', async (request, reply) => {
-    const remedioId = request.params.id
-
-    await database.delete(Number(remedioId))
-
-    return reply.status(204).send()
-})
 
 app.register(userRoutes);
+app.register(medicationsRoutes);
 
 app.listen({host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 3333})
